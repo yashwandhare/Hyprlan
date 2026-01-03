5a44962fcd2f9acac4b28386dc552af29b23d3a0
yashwandhare <wandhareyash7@gmail.com>
2026-01-04 01:20:28 +0530

    improved control pannel

--- hypr/scripts/control-panel.sh
+++ hypr/scripts/control-panel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-choice=$(printf "󰖩  Wi-Fi\n󰂯  Bluetooth\n󰂛  Do Not Disturb\n  Software Store\n  KDE Connect\n  Config" | \
-    wofi --dmenu --prompt "" --width=300 --height=250 --location=center --hide-scroll --no-actions)
+choice=$(printf "󰖩  Wi-Fi\n󰂯  Bluetooth\n󰂛  Do Not Disturb\n󰀵  Software Store\n󰄜  KDE Connect\n󰒍  LocalSend\n󰆍  Config" | \
+    wofi --dmenu --prompt "" --width=300 --height=280 --location=center --hide-scroll --no-actions)
 
 case "$choice" in
   "󰖩  Wi-Fi")
@@ -13,16 +13,21 @@ case "$choice" in
   "󰂛  Do Not Disturb")
     ~/.config/hyprland/hypr/scripts/toggle-dnd.sh
     ;;
-  "  Software Store")
+  "󰀵  Software Store")
     flatpak run org.gnome.Software 2>/dev/null || \
     gnome-software 2>/dev/null || \
     discover 2>/dev/null || \
     notify-send "Software Store" "Software store not found"
     ;;
-  "  KDE Connect")
+  "󰄜  KDE Connect")
     kdeconnect-app &
     ;;
-  "  Config")
+  "󰒍  LocalSend")
+    flatpak run org.localsend.localsend_app 2>/dev/null || \
+    localsend_app 2>/dev/null || \
+    notify-send "LocalSend" "LocalSend not found"
+    ;;
+  "󰆍  Config")
     cd ~/.config/hyprland && code . &
     ;;
 esac
