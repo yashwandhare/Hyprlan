86106923c64c134b5af897051c5cb2ae61aa14ed
yashwandhare <wandhareyash7@gmail.com>
2026-01-11 16:31:01 +0530

    bug fizes

--- waybar/scripts/screen-record.sh
+++ waybar/scripts/screen-record.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if pgrep -f "gpu-screen-recorder|obs|ffmpeg|wf-recorder|screenkey" > /dev/null 2>&1; then
+if pgrep -f "gpu-screen-recorder|^obs$|ffmpeg|wf-recorder|screenkey" > /dev/null 2>&1; then
     echo "󰻃 REC"
 else
     echo ""
